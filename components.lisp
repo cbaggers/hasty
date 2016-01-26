3f932b38f16ec66c58f129667e13e0c3523fbdbb
Baggers <chris.bagley@gmail.com>
2016-01-26 19:07:30 +0100

    Remove access controls
    Have done this for a two main reasons
    
    - It felt bad. It was way to awkward to work from the repl
    - It was too easy to ignore: often you store a stateful object in a
      component's slot. At that point you are only stopping people from
      changing the first level..which is daft
    
    With those two we kill the reason for locking things down so
    tightly. However, we still keep `depends-on` because we get to say:
    > "you can access this data from anywhere, but if you want to be robust, access
    >  it from the correct system. Otherwise all bets are off"
    
    This feels more sensible.

--- components.lisp
+++ components.lisp
@@ -12,8 +12,9 @@
 
 (defvar debug-id-source -1)
 
-(defun hidden-name (x)
-  (intern (format nil "~a-~a" (package-name (symbol-package x)) (symbol-name x))
+(defun hidden-name (&rest x)
+  (intern (format nil "~a~{-~a~}" (package-name (symbol-package (first x)))
+		  (mapcar #'symbol-name x))
 	  (find-package :hasty-hidden)))
 
 (defmacro def-component (name-and-options depends-on (&rest slot-descriptions)
@@ -30,13 +31,12 @@
     (assert (valid-slot-descriptions-p slot-descriptions))
     (let* ((system-name (symb name :-system))
 	   (payload (symb :% name :-payload))
-	   (payload-type (symb name :-payload))
+	   (payload-type (hidden-name (symb name :-payload)))
 
 	   (reactive (eq (first depends-on) :reactive))
 	   (friends (if reactive (rest depends-on) depends-on))
 
-	   (let-id (symb :%- (format nil "~{~s~^-~}"
-				     (map 'list #'char-code (symbol-name name)))))
+	   (let-id (hidden-name (symb name :-id)))
 	   (id (gensym "id"))
 
 	   (init (symb :make- name))
@@ -46,25 +46,31 @@
 	   (with (symb :with- name))
 
 	   (update (symb :update))
+	   (update-component (symb :update- name))
 	   (has (symb :has- name))
 	   (add (symb :add- name))
 	   (remove (symb :remove- name))
 
 	   (original-slot-names (mapcar #'first slot-descriptions))
-	   (hidden-slot-names (mapcar #'hidden-name original-slot-names))
 
-	   (init-args (mapcar λ`(,_ ,(second _1))
-			      original-slot-names
-			      slot-descriptions))
-	   (init-pairs (mapcan λ`(,(kwd _) ,_1)
-			       hidden-slot-names
-			       original-slot-names))
+	   (slot-names (mapcar λ(symb :% name :- (first _))
+			       slot-descriptions))
+
+	   (getter-names (mapcar λ(symb name :- (first _))
+			       slot-descriptions))
+
+	   (add-args (mapcar λ`(,_ ,(second _1))
+			     original-slot-names
+			     slot-descriptions))
+	   (add-pairs (mapcan λ`(,(kwd _) ,_1)
+			      slot-names
+			      original-slot-names))
 
 	   (non-opt-init-args (mapcar λ`(,_ ,(second _1))
-				      hidden-slot-names
+				      slot-names
 				      slot-descriptions))
 	   (non-opt-init-pairs (mapcan λ`(,(kwd _) ,_)
-				       hidden-slot-names))
+				       slot-names))
 
 	   (update-args (mapcar λ`(,_ nil ,(symb :set _)) original-slot-names))
 
@@ -78,19 +84,19 @@
 		 `((defclass ,payload-type ()
 		     ,(mapcar λ`(,_ :initarg ,(kwd _)
 				    :initform ,(second _1))
-			      hidden-slot-names
+			      slot-names
 			      slot-descriptions))
 		   ,@(mapcar λ`(defun ,_ (x)
 				 (slot-value
 				  (slot-value x ',payload)
 				  ',_))
-			     hidden-slot-names)
+			     slot-names)
 		   ,@(mapcar λ`(defun (setf ,_) (v x)
 				 (setf (slot-value
 					(slot-value x ',payload)
 					',_)
 				       v))
-			     hidden-slot-names)
+			     slot-names)
 		   (defun ,init (&key ,@non-opt-init-args)
 		     (,non-optimal-init
 		      ,(kwd payload) (make-instance
@@ -102,7 +108,9 @@
 			   ,@(when (not optimize)
 				   `((:constructor ,non-optimal-init))))
 	   ,@(if optimize
-		 (mapcar λ`(,_ ,@(rest _1)) hidden-slot-names slot-descriptions)
+		 (mapcar λ`(,_ ,@(rest _1))
+			 slot-names
+			 slot-descriptions)
 		 `((,payload (make-instance ',payload-type)
 			     :type ,payload-type))))
 
@@ -116,15 +124,7 @@
 			      (symbol-value ',let-id)
 			      (%next-id)))))
 
-	   ;; local get from entity helper func {TODO} should inline this
-	   (labels ((%get-from (entity)
-		      (get-item-from-%component-bag-at
-		       (entity-components entity) ,id)))
-
-	     ;; make public getters as component getters are hidden
-	     ,@(mapcar λ`(defun ,(symb name :- _) (entity) (,_1 (%get-from entity)))
-		       original-slot-names hidden-slot-names)
-
+	   (progn
 	     (defmacro ,with (slot-names entity &body body)
 	       (unless (and (listp slot-names)
 			    (>= (length slot-names) 1)
@@ -132,15 +132,35 @@
 				   slot-names))
 		 (error ,(format nil "~%~s:~%Invalid slot names. Must be a list of one or more of the following:~%~s~%You gave~~s" with original-slot-names)
 			slot-names))
-	       (gen-slot-accessors entity slot-names ',original-slot-names
-				   ',hidden-slot-names body ,id))
+	       (let ((inst (gensym "inst"))
+		     (lookup ',(mapcar #'cons original-slot-names slot-names)))
+		 `(let* ((,inst (get-item-from-%component-bag-at
+				    (entity-components ,entity) ,,id)))
+		    (declare (ignorable ,inst))
+		    (symbol-macrolet ,(mapcar λ`(,_ (,(cdr (assoc _ lookup))
+						      ,inst))
+					      slot-names)
+		      ,@body))))
+
+	     ,@(mapcar λ`(defun ,_ (entity)
+			   (,_1 (get-item-from-%component-bag-at
+				 (entity-components entity) ,id)))
+		       getter-names
+		       slot-names)
+
+	     ,@(mapcar λ`(defun (setf ,_) (value entity)
+			   (setf (,_1 (get-item-from-%component-bag-at
+				       (entity-components entity) ,id))
+				 value))
+		       getter-names
+		       slot-names)
 
 	     (defun ,has (entity)
 	       (has-item-in-%component-bag-at
 		(entity-components entity) ,id))
 
-	     (defun ,add (entity-to-add-to &key ,@init-args)
-	       (let ((component (,init ,@init-pairs)))
+	     (defun ,add (entity-to-add-to &key ,@add-args)
+	       (let ((component (,init ,@add-pairs)))
 		 (add-item-to-%component-bag-at
 		  (entity-components entity-to-add-to) component ,id))
 	       (unless-release
@@ -179,23 +199,32 @@
 	     (defmethod %get-friends ((component ,name))
 	       ',friends)
 
+	     (defun ,update-component(entity &key ,@update-args)
+	       (let ((,component (get-item-from-%component-bag-at
+				  (entity-components entity) ,id)))
+		 ,@(mapcar
+		    λ`(when ,(third _1)
+			(setf (,_ ,component) ,(first _1)))
+		    slot-names
+		    update-args)))
+
 	     (defun ,pass (,entity)
 	       ;; cant use our with-macro from earlier as it's not yet compiled
 	       ;; so we locally define it here and use it immediately
-	       (macrolet ((,with (,entity &body body)
-			    (gen-slot-accessors
-			     ,entity ',original-slot-names ',original-slot-names
-			     ',hidden-slot-names body ',id)))
-		 (let ((,component (%get-from ,entity)))
-		   (,with ,entity
-			  (labels ((,update (&key ,@update-args)
-				     ,@(mapcar
-					λ`(when ,(third _1)
-					    (setf (,_ ,component) ,(first _1)))
-					hidden-slot-names
-					update-args)))
-			    (declare (ignorable (function ,update)))
-			    ,@pass-body)))))
+	       (let ((,component (get-item-from-%component-bag-at
+				  (entity-components ,entity) ,id)))
+		 (symbol-macrolet ,(mapcar λ`(,_ (,_1 ,component))
+					   original-slot-names
+					   slot-names)
+
+		   (labels ((,update (&key ,@update-args)
+			      ,@(mapcar
+				 λ`(when ,(third _1)
+				     (setf (,_ ,component) ,(first _1)))
+				 slot-names
+				 update-args)))
+		     (declare (ignorable (function ,update)))
+		     ,@pass-body))))
 
 	     (let ((created nil))
 	       (defun ,system-init ()
@@ -220,34 +249,6 @@
 		 (,get-system))
 	       (defmethod get-system ((name ,name))
 		 (,get-system)))))))))
-
-(defun gen-slot-accessors (entity needed-slots human-slot-names
-			   gensym-slot-names body id)
-  (let* ((name-pairs (mapcar #'list human-slot-names gensym-slot-names))
-	 (needed (intersection name-pairs needed-slots
-			       :test λ(eq (car _) _1)))
-	 (human-slot-names (mapcar #'first needed))
-	 (gensym-slot-names (mapcar #'second needed))
-	 (gensym-func-names (mapcar λ(gensym (symbol-name _))
-				    human-slot-names))
-
-	 (c-inst (gensym "component")))
-    ;; get this component from the entity
-    `(let* ((,c-inst (get-item-from-%component-bag-at
-		      (entity-components ,entity) ,id)))
-       (declare (ignorable ,c-inst))
-       ;; make the functions to access the slots, we need this
-       ;; as otherwise you could setf
-       (labels ,(mapcar λ`(,_ () (,_1 ,c-inst))
-			gensym-func-names gensym-slot-names)
-	 (declare (ignorable
-		   ,@(mapcar λ`(function ,_) gensym-func-names)))
-	 ;; make symbol macros so we can use the above functions like
-	 ;; symbols again
-	 (symbol-macrolet ,(mapcar λ`(,_ (,_1))
-				   human-slot-names
-				   gensym-func-names)
-	   ,@body)))))
 
 (defun valid-slot-descriptions-p (slot-descriptions)
   (labels ((valid-slot (s)
